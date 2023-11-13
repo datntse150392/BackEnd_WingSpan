812b4cba82f9afa16320ae00f62464f2030a25b2
Nguyen Thanh Dat <thuykhangvn@gmail.com>
2023-11-14 02:09:52 +0700

    upload again

--- src/app/controllers/course.controller.js
+++ src/app/controllers/course.controller.js
@@ -11,4 +11,11 @@ module.exports = {
       data: courses,
     });
   },
+  // getCourses: async (req, res, next) => {
+  //   const courses = await courseSchema.find({});
+  //   return res.status(200).json({
+  //     status: 200,
+  //     data: courses,
+  //   });
+  // },
 };
