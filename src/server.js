ca8caf4085813ec88844cdc678c7647f230cdbc8
Nguyen Thanh Dat <thuykhangvn@gmail.com>
2023-11-20 03:37:01 +0700

    upload api service

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const cors = require("cors");
 const createProxyMiddleware = require("http-proxy-middleware");
 const express = require("express");
-const routers = require("./src/app/routes");
+const routers = require("./app/routes");
 // Kết nối đến MongoDB
-const connectDB = require("./src/app/configs/database");
+const connectDB = require("./app/configs/database");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
